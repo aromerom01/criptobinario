aa7ea222e75eef1e41b966baf7e3146c4ef54569
melissaforero <melissa.forero@escom.edu.co>
2020-11-04 19:40:21 -0500

    Se agrega Menu y se añade la clase de cifrar

--- src/javacriptobinario/Javacriptobinario.java
+++ src/javacriptobinario/Javacriptobinario.java
@@ -17,36 +17,17 @@ public class Javacriptobinario {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         // TODO code application logic here
-String sMensaje = JOptionPane.showInputDialog("Cifrado de CriptoBinario\n\nPara iniciar ingrese el mensaje:");
-        String binario="";
-        for (int j=0;j < sMensaje.length();j++){
-                char EXT = sMensaje.charAt(j);
-                
-                //System.out.print(matrizGronsfeld [fila][columna]);
-                
-            
-        char characterValue = EXT;
-        int asciiValue = (int) characterValue;
-        System.out.println(asciiValue);
+        Cifrar cifrar = new Cifrar();
         
-        for (int i=7;i >= 0;i--)
-        {
-            double a= Math.pow (2, i);
-            int x = (int) a;
-            
-            System.out.println(x);
-                if (asciiValue >= x){
-                    binario = binario + 1;
-                    asciiValue  = asciiValue - x;
-                }
-                else{
-                    binario= binario+0;
-                }
-        
-    }
-        System.out.println(binario);
-    
-}
-}
+       cifrar.sMensaje = JOptionPane.showInputDialog("Cifrado de CriptoBinario\n\nPara iniciar ingrese el mensaje:");
+       String ingreso = JOptionPane.showInputDialog("Para cifrar ingrese 1 o Para descifrar 2:");
+       int accion = Integer.parseInt(ingreso);
+       if (accion==1){
+     
+              
+       cifrar.cifrado();
+       }
+       }
 }
